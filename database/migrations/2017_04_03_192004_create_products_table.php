15ddb9d4f30d8eecd7be231f26e353ee9d01f664
Alexandre dos Santos Andrade <alexandre@it.art.br>
2017-04-03 16:34:34 -0300

    Finalizando Modelos

---
+++ database/migrations/2017_04_03_192004_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->name('name')->index();
+            $table->longText('descritpion');
+            $table->double('max_price');
+            $table->double('min_price');
+            $table->json('prices');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
